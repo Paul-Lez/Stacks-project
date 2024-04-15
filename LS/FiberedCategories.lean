1c6ddec5f31d6c163116a1435fc822ef46a5d98c
Calle Sönne <calle.sonne@gmail.com>
2024-04-15 22:20:53 +0100

    alphabetic name ordering

--- LS/FiberedCategories.lean
+++ LS/FiberedCategories.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2024 Calle Sönne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Calle Sönne, Paul Lezeau
+Authors: Paul Lezeau, Calle Sönne
 -/
 
 import Mathlib.CategoryTheory.Functor.Category
