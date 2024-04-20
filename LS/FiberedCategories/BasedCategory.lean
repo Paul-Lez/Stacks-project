cc0d40bd937cdc8c9c37dda0b8557f3d5e211e58
Paul-Lez <paul.lezeau@gmail.com>
2024-04-20 10:03:54 +0200

    Add copyright headers

--- LS/FiberedCategories/BasedCategory.lean
+++ LS/FiberedCategories/BasedCategory.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2024 Calle Sönne. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Paul Lezeau, Calle Sönne
+-/
+
 import LS.FiberedCategories.Basic
 import Mathlib.CategoryTheory.Bicategory.Strict
 import Mathlib.CategoryTheory.Functor.Category
