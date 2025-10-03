86c62bf13a2ca046bac0eae30047a03c6e6d7782
Paul-Lez <paul.lezeau@gmail.com>
2025-10-03 14:49:27 +0100

    Update lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -1,6 +1,12 @@
 import Lake
 open Lake DSL
 
+abbrev mathlibLeanOptions : Array LeanOption := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`autoImplicit, false⟩,
+    ⟨`maxSynthPendingDepth, .ofNat 3⟩
+  ]
+
 package «my_project» {
   -- add any package configuration options here
 }
@@ -9,6 +15,5 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "v4.24.0-rc1"
 
 @[default_target]
-lean_lib «StacksProject» {
-  -- add any library configuration options here
-}
+lean_lib StacksProject where
+  leanOptions := mathlibLeanOptions
